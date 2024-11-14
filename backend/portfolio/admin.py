d99515c64e3f455a28d4ec706d08ef6b6eabbaa8
LuchoProgramer <luchoviteri1990@gmail.com>
2024-11-14 11:28:33 -0500

    Eliminar backend y preparar para CMS sin cabeza

--- backend/portfolio/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Blog
-
-@admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'fecha_publicacion', 'autor')
-    search_fields = ('titulo', 'contenido', 'categorias')
